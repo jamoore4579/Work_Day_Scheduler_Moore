6d4e274abe9f3c3d7bbd65dadc852541372c3bf8
Irish Moore <jasonamoore@frontier.com>
2021-12-30 14:57:53 -0500

    determine if time is past or future

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,35 @@
-//New File
-
 // Todays date
 $("#currentDay").text(moment().format("dddd, MMMM Do"));
+
+
+// determine if past, present or future
+var checkTime = function() {
+    var hour = $(".hour").text().trim();
+
+    var time = moment(hour, "LT");
+    
+    // emove any old classes from element
+    $(".hour").removeClass(".present .past .future");
+
+    // apply new class if time is past/future time
+    if (moment().isAfter(time)) {
+        $(".hour").addClass(".past");
+    } else if (moment().isBefore(time)) {
+        $(".hour").addClass(".future");
+    } else {
+        $(".hour").addClass(".present");
+    }
+}
+
+checkTime();
+
+
+
+
+
+  
+
+    
+
+
+
