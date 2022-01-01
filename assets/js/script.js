df580c9046e8322eee388e1879b36aac3237e857
Irish Moore <jasonamoore@frontier.com>
2021-12-31 20:55:27 -0500

    classes are working

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,11 +15,13 @@ $(document).ready(function() {
     function scheduler() {
         // determine current time
         var currentTime = moment().hour();
+        console.log(currentTime)
 
         // loop through time options
         $(".time-block").each(function() {
             var timeSeg = parseInt($(this).attr("id").split("hour")[1]);
-            
+            console.log(timeSeg)
+
             // apply correct class per time reference
             if (timeSeg < currentTime) {
                 $(this).removeClass("future");
@@ -29,26 +31,26 @@ $(document).ready(function() {
             else if (timeSeg === currentTime) {
                 $(this).removeClass("past");
                 $(this).removeClass("future");
-                $(this).addClass("present")
+                $(this).addClass("present");
             }
             else {
-                $(this).removeClass("past")
-                $(this).removeClass("Present")
-                $(this).addClass("future")
+                $(this).removeClass("past");
+                $(this).removeClass("present");
+                $(this).addClass("future");
             }
         })
     }
 
     // retrieve data from local storage
-    $("#9 .description").val(localStorage.getItem("9"));
-    $("#10 .description").val(localStorage.getItem("10"));
-    $("#11 .description").val(localStorage.getItem("11"));
-    $("#12 .description").val(localStorage.getItem("12"));
-    $("#13 .description").val(localStorage.getItem("13"));
-    $("#14 .description").val(localStorage.getItem("14"));
-    $("#15 .description").val(localStorage.getItem("15"));
-    $("#16 .description").val(localStorage.getItem("16"));
-    $("#17 .description").val(localStorage.getItem("17"));
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
 
     scheduler();
 })
